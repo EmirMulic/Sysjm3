2c1791dbd0246c6418f0c0ce4c829b13798079a5
EmirMulic <45227727+EmirMulic@users.noreply.github.com>
2020-05-19 10:05:51 +0200

    Add files via upload

---
+++ Sysjm3/src/main/java/Stem/Sysjm3/Sysjm3Application.java
@@ -0,0 +1,13 @@
+package Stem.Sysjm3;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Sysjm3Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Sysjm3Application.class, args);
+	}
+
+}
---
+++ Sysjm3/src/test/java/Stem/Sysjm3/Sysjm3ApplicationTests.java
@@ -0,0 +1,13 @@
+package Stem.Sysjm3;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Sysjm3ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
